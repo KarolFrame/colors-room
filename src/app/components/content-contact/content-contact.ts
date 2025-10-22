480d63e921e2cdd59c06051b7fb3e38922ecf455
Carolina <carolina.peseca@gmail.com>
2025-10-22 13:50:35 +0200

    time animations

--- src/app/components/content-contact/content-contact.ts
+++ src/app/components/content-contact/content-contact.ts
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
           style({ transform: 'translateX(-100%)', opacity: 0 })
         ], { optional: true }),
         query('*', [
-          stagger('300ms', [
+          stagger('150ms', [
             animate('800ms cubic-bezier(0.25, 0.8, 0.5, 1)', style({ transform: 'translateX(0)', opacity: 1 }))
           ])
         ], { optional: true })
